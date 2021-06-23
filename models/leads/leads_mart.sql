b8c3c3be0ac856751a0de1834093c18b87b5bc70
Denys Arysmiatov <arismiatov@gmail.com>
2021-06-23 06:43:26 +0000

    tra upd

--- models/leads/leads_mart.sql
+++ models/leads/leads_mart.sql
@@ -8,7 +8,7 @@ transactions as ( select * from {{ ref('stg_transactions') }})
 select
     l.*,
     CASE 
-        WHEN EXISTS (SELECT 1 FROM transactions t WHERE t.Lead_Id = l.id) and l.ProjectStatus != "2" THEN 1 
+        WHEN EXISTS (SELECT 1 FROM transactions t WHERE t.Lead_Id = l.id) and l.ProjectStatus != "Free" THEN 1 
         ELSE 0 
     END as Billable
 from leads l
