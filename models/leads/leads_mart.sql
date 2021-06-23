5a470c44425f9fd8515b7718d01eb8e5d3f60886
Denys Arysmiatov <arismiatov@gmail.com>
2021-06-23 06:27:37 +0000

    transactions added

---
+++ models/leads/leads_mart.sql
@@ -0,0 +1,13 @@
+{{ config ( 
+    materialized="table" 
+)}}
+
+with leads as ( select * from {{ ref('stg_leads') }})
+
+with transactions as ( select * from {{ ref('stg_transactions') }})
+
+select
+    l.*,
+    CAST(CASE WHEN EXISTS (SELECT 1 FROM transactions t WHERE t.Lead_Id = l.id) and l.ProjectStatus != 2 THEN 1 ELSE 0 END as bit) as Billable
+from leads l
+
