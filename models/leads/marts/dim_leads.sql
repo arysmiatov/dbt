a94406eb401fa27ba237deadcad9dd66dab99acf
Denys Arysmiatov <arismiatov@gmail.com>
2021-06-24 21:14:22 +0000

    change incr

--- models/leads/marts/dim_leads.sql
+++ models/leads/marts/dim_leads.sql
@@ -1,8 +1,10 @@
-{{ config(
-    materialized='table'
+{{config(
+    materialized = 'incremental'
 )}}
+
 with leads as ( select * from {{ ref('stg_leads') }}),
 transactions as ( select * from {{ ref('stg_transactions') }})
+
 select
     l.*,
     CASE 
@@ -10,3 +12,10 @@ select
         ELSE 0 
     END as Billable
 from leads l
+
+{% if is_incremental() %}
+
+  -- this filter will only be applied on an incremental run
+  where l.Date > (select max(l.Date) from {{ this }})
+
+{% endif %}
