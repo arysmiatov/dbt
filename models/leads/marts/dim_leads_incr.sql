8127b3b8055048febe6312cdd3f5a894d2f01f3e
Denys Arysmiatov <arismiatov@gmail.com>
2021-06-25 05:32:18 +0000

    test new incr model

---
+++ models/leads/marts/dim_leads_incr.sql
@@ -0,0 +1,26 @@
+{{ config(
+    materialized='table',
+    partition_by={
+      "field": "date",
+      "data_type": "timestamp",
+      "granularity": "day"
+    }
+)}}
+
+with leads as ( select * from {{ ref('stg_leads') }}),
+transactions as ( select * from {{ ref('stg_transactions') }})
+
+select
+    l.*,
+    CASE 
+        WHEN EXISTS (SELECT 1 FROM transactions t WHERE t.Lead_Id = l.id) and l.ProjectStatus != "Free" THEN 1 
+        ELSE 0 
+    END as Billable
+from leads l
+
+-- {% if is_incremental() %}
+
+--   -- this filter will only be applied on an incremental run
+--   where l.Date > (select max(l.Date) from {{ this }})
+
+-- {% endif %}
